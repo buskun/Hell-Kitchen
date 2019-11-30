87569de36ccf7e07aecccb1ebe4ccbeac7294502
Sittikorn Hirunpongthawat <buskun.sittikorn@gmail.com>
2019-11-30 17:57:14 +0700

    Add example

--- src/game/scenes/LoadingScene.java
+++ src/game/scenes/LoadingScene.java
@@ -3,6 +3,8 @@ package game.scenes;
 import base.Controller;
 import base.Scene;
 import base.Window;
+import utility.StateManager;
+import utility.Utility;
 
 public class LoadingScene extends Scene {
     public LoadingScene(Window window, Controller controller) { super(window, controller); }
@@ -10,6 +12,20 @@ public class LoadingScene extends Scene {
     @Override
     public void init() {
         changeBackground("resources/background.jpg");
+
+        StateManager<Integer> iconState = Utility.useState(0);
+
+        Utility.setInterval(() -> {
+            if (iconState.get() == 0) {
+                changeBackground("resources/images/base/fallback.png");
+                iconState.set(1);
+            } else if (iconState.get() == 1) {
+                changeBackground("resources/background.jpg");
+                iconState.set(0);
+            }
+
+            return true;
+        }, 1000);
 
         ready();
     }
