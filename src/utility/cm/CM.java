c94461ff316904410199ff707d89fe653640097e
Sittikorn Hirunpongthawat <buskun.sittikorn@gmail.com>
2019-12-21 19:15:43 +0700

    Optimize CM

--- src/utility/cm/CM.java
+++ src/utility/cm/CM.java
@@ -2,8 +2,11 @@ package utility.cm;
 
 import base.Scene;
 import components.CustomImageIcon;
+import utility.StateManager;
+import utility.Utility;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
@@ -97,13 +100,15 @@ public class CM extends ComponentAdapter {
     }
 
     private Scene cScene;
+    private final ArrayList<JComponent> calculationComponentList = new ArrayList<>();
     private ArrayList<CMData<PointStore, ?>> pointCalculationList = new ArrayList<>();
     private ArrayList<CMData<DimensionStore, ?>> dimensionCalculationList = new ArrayList<>();
     private ArrayList<CMData<DimensionStore, CustomImageIcon>> iconCalculationList = new ArrayList<>();
+    private boolean ready = false;
+    private ArrayList<Runnable> recalculationQueue = new ArrayList<>();
 
     public CM(Scene scene) {
         cScene = scene;
-
         cScene.addComponentListener(this);
     }
 
@@ -113,8 +118,6 @@ public class CM extends ComponentAdapter {
     }
 
     public void setSize(JComponent component, DimensionStore dimension) {
-        component.setSize(dimension.calculate(cScene.getWidth(), cScene.getHeight()));
-
         boolean foundDim = false;
         for (CMData<DimensionStore, ?> dimData : dimensionCalculationList) {
             if (dimData.is(component)) {
@@ -124,7 +127,11 @@ public class CM extends ComponentAdapter {
             }
         }
 
-        if (!foundDim) dimensionCalculationList.add(new CMData<>(component, dimension));
+        if (!calculationComponentList.contains(component)) calculationComponentList.add(component);
+
+        if (!foundDim) {
+            dimensionCalculationList.add(new CMData<>(component, dimension));
+        }
     }
 
     public DimensionStore getScaledSize(JComponent component) {
@@ -138,8 +145,6 @@ public class CM extends ComponentAdapter {
     }
 
     public void setLocation(JComponent component, PointStore location) {
-        component.setLocation(location.calculate(cScene.getWidth(), cScene.getHeight()));
-
         boolean foundPoint = false;
         for (CMData<PointStore, ?> pointData : pointCalculationList) {
             if (pointData.is(component)) {
@@ -149,7 +154,11 @@ public class CM extends ComponentAdapter {
             }
         }
 
-        if (!foundPoint) pointCalculationList.add(new CMData<>(component, location));
+        if (!calculationComponentList.contains(component)) calculationComponentList.add(component);
+
+        if (!foundPoint) {
+            pointCalculationList.add(new CMData<>(component, location));
+        }
     }
 
     public PointStore getScaledLocation(JComponent component) {
@@ -163,10 +172,6 @@ public class CM extends ComponentAdapter {
     }
 
     public void setIcon(JComponent component, CustomImageIcon icon, DimensionStore size) {
-        try {
-            component.getClass().getMethod("setIcon", Icon.class).invoke(component, icon.scaleToFill(component.getSize()));
-        } catch (Exception ignored) {}
-
         boolean foundIcon = false;
         for (CMData<DimensionStore, CustomImageIcon> iconData : iconCalculationList) {
             if (iconData.is(component)) {
@@ -214,53 +219,6 @@ public class CM extends ComponentAdapter {
         return null;
     }
 
-    public void recalculate(JComponent component) {
-        int width = cScene.getWidth();
-        int height = cScene.getHeight();
-
-        for (CMData<PointStore, ?> pointData : pointCalculationList) {
-            if (pointData.is(component)) {
-                pointData.getComponent().setLocation(pointData.getStore().calculate(width, height));
-                break;
-            }
-        }
-
-        for (CMData<DimensionStore, ?> dimData : dimensionCalculationList) {
-            if (dimData.is(component)) {
-                dimData.getComponent().setSize(dimData.getStore().calculate(width, height));
-                break;
-            }
-        }
-
-        for (CMData<DimensionStore, CustomImageIcon> iconData : iconCalculationList) {
-            if (iconData.is(component)) {
-                try {
-                    component.getClass().getMethod("setIcon", Icon.class)
-                            .invoke(component, iconData.getOtherData().resize(
-                                    iconData.getStore().calculate(width, height)
-                            ));
-                } catch (Exception ignored) {}
-                break;
-            }
-        }
-    }
-
-    public void recalculate() {
-        int width = cScene.getWidth();
-        int height = cScene.getHeight();
-
-        pointCalculationList.forEach(pointData -> pointData.getComponent().setLocation(pointData.getStore().calculate(width, height)));
-        dimensionCalculationList.forEach(dimData -> dimData.getComponent().setSize(dimData.getStore().calculate(width, height)));
-        iconCalculationList.forEach(iconData -> {
-            try {
-                iconData.getComponent().getClass().getMethod("setIcon", Icon.class)
-                        .invoke(iconData.getComponent(), iconData.getOtherData().resize(
-                                iconData.getStore().calculate(width, height)
-                        ));
-            } catch (Exception ignored) {}
-        });
-    }
-
     public void setHoverIcon(JComponent component, CustomImageIcon normalIcon, CustomImageIcon hoverIcon) {
         component.addMouseListener(new MouseAdapter() {
             @Override
@@ -301,10 +259,118 @@ public class CM extends ComponentAdapter {
         });
     }
 
+    synchronized public void recalculate(JComponent component) {
+        ready = false;
+        int width = cScene.getWindow().getWidth();
+        int height = cScene.getWindow().getHeight();
+
+        for (CMData<PointStore, ?> pointData : pointCalculationList) {
+            if (pointData.is(component)) {
+                pointData.getComponent().setLocation(pointData.getStore().calculate(width, height));
+                break;
+            }
+        }
+
+        for (CMData<DimensionStore, ?> dimData : dimensionCalculationList) {
+            if (dimData.is(component)) {
+                dimData.getComponent().setSize(dimData.getStore().calculate(width, height));
+                break;
+            }
+        }
+
+        for (CMData<DimensionStore, CustomImageIcon> iconData : iconCalculationList) {
+            if (iconData.is(component)) {
+                try {
+                    component.getClass().getMethod("setIcon", Icon.class)
+                            .invoke(component, iconData.getOtherData().resize(
+                                    iconData.getStore().calculate(width, height)
+                            ));
+                } catch (Exception ignored) {}
+                break;
+            }
+        }
+        ready = true;
+    }
+
+    public void recalculate() {
+        ready = false;
+        int width = cScene.getWindow().getWidth();
+        int height = cScene.getWindow().getHeight();
+
+        ArrayList<Runnable> queue = new ArrayList<>();
+
+        synchronized (calculationComponentList) {
+            calculationComponentList.forEach(component -> {
+                Rectangle bound = component.getBounds();
+
+                StateManager<Boolean> edited = Utility.useState(false);
+                dimensionCalculationList.stream().filter(cm -> cm.getComponent() == component).findFirst().ifPresent(cm -> {
+                    Dimension newSize = cm.getStore().calculate(width, height);
+
+                    if (bound.getWidth() != newSize.getWidth() || bound.getHeight() != newSize.getHeight()) {
+                        bound.setSize(newSize);
+                        edited.set(true);
+                    }
+                });
+
+                pointCalculationList.stream().filter(cm -> cm.getComponent() == component).findFirst().ifPresent(cm -> {
+                    Point newLocation = cm.getStore().calculate(width, height);
+
+                    if (bound.getY() != newLocation.getX() || bound.getX() != newLocation.getY()) {
+                        bound.setLocation(newLocation);
+                        edited.set(true);
+                    }
+                });
+
+                if (edited.get()) queue.add(() -> component.setBounds(bound));
+
+                iconCalculationList.stream().filter(cm -> cm.getComponent() == component).findFirst().ifPresent(cm -> {
+                    Dimension newSize = cm.getStore().calculate(width, height);
+
+                    try {
+                        Icon icon = (Icon) cm.getComponent().getClass().getMethod("getIcon").invoke(cm.getComponent());
+                        if (icon == null || icon.getIconHeight() != newSize.getHeight() || icon.getIconWidth() != newSize.getWidth()) {
+                            queue.add(() -> {
+                                try {
+                                    cm.getComponent().getClass().getMethod("setIcon", Icon.class)
+                                            .invoke(cm.getComponent(), cm.getOtherData().resize(newSize));
+                                } catch (Exception ignored) {}
+                            });
+                        }
+                    } catch (Exception ignored) { }
+                });
+            });
+        }
+
+        cScene.setVisible(false);
+        queue.forEach(Runnable::run);
+        cScene.setVisible(true);
+
+        ready = true;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
     @Override
     public void componentResized(ComponentEvent e) {
         super.componentResized(e);
 
-        recalculate();
+        if (!recalculationQueue.isEmpty()) {
+            recalculationQueue.forEach(Runnable::run);
+            recalculationQueue = new ArrayList<>();
+        }
+
+        recalculationQueue.add(Utility.setTimeout(() -> {
+            Scene loadingScene = cScene.getController().getLoadingScene();
+            cScene.setVisible(false);
+            if (loadingScene != null) loadingScene.start();
+
+            recalculate();
+
+            if (loadingScene != null) loadingScene.stop();
+            cScene.setVisible(true);
+        }, 100));
     }
 }
