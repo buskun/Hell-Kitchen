d5ba70d5524925e8cd0ac26e51d385b1dd55b785
Sittikorn Hirunpongthawat <buskun.sittikorn@gmail.com>
2019-12-21 19:16:16 +0700

    Temp

--- src/base/Window.java
+++ src/base/Window.java
@@ -10,6 +10,7 @@ public class Window extends JFrame {
     public Window(String title, Dimension size, Point position) {
         setTitle(title);
         setBounds(position.x, position.y, size.width, size.height);
+        setResizable(false);
 
         getContentPane().setLayout(new BorderLayout());
 
