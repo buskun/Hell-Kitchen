42c6cc1f9f32861ff39ecf0c8379c1b42c8fbd14
Sittikorn Hirunpongthawat <buskun.sittikorn@gmail.com>
2019-12-22 18:52:02 +0700

    Add Refrigerator frame

---
+++ src/game/frames/RefrigeratorFrame.java
@@ -0,0 +1,98 @@
+package game.frames;
+
+import components.CustomImageIcon;
+import utility.cm.CM;
+import utility.cm.CMFlag;
+import utility.loader.AudioLoader;
+import utility.loader.ImageLoader;
+
+import javax.swing.*;
+
+public class RefrigeratorFrame extends JFrame {
+    static final int width = 1200;
+    static final int height = 900;
+
+    public RefrigeratorFrame(ImageLoader imageLoader, AudioLoader audioLoader) {
+        setTitle("Refrigerator");
+        setBounds(100, 100, width, height);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        CM cm = new CM(() -> this, visible -> {}, () -> null, this::addComponentListener);
+
+        JLabel contentPane = new JLabel();
+        contentPane.setBounds(0, 0, width, height);
+        contentPane.setLayout(null);
+        setContentPane(contentPane);
+
+        cm.setIcon(contentPane, imageLoader.getIcon("backgroundFrame"), CM.size(100, 100));
+        cm.setBounds(contentPane, CM.grid(0, 0, 100, 100));
+        contentPane.setLayout(null);
+
+        JButton Burger = new JButton();
+        cm.setIcon(Burger, imageLoader.getIcon("Burger"), CM.size(30, CMFlag.BY_H));
+        cm.setBounds(Burger, CM.grid(30, 50, CM.size(30, CMFlag.BY_H)));
+        Burger.setBounds(0, 0, 100, 200);
+        Burger.setOpaque(false);
+        Burger.setContentAreaFilled(false);
+        Burger.setBorderPainted(false);
+        contentPane.add(Burger);
+
+        JButton Fish = new JButton();
+        cm.setIcon(Fish, imageLoader.getIcon("Fish"), CM.size(20, CMFlag.BY_H));
+        cm.setBounds(Fish, CM.grid(48, 5, CM.size(20, CMFlag.BY_H)));
+        Fish.setOpaque(false);
+        Fish.setContentAreaFilled(false);
+        Fish.setBorderPainted(false);
+        contentPane.add(Fish);
+
+        JButton Ketchup = new JButton();
+        cm.setIcon(Ketchup, imageLoader.getIcon("Ketchup"), CM.size(20, CMFlag.BY_H));
+        cm.setBounds(Ketchup, CM.grid(30, 32, CM.size(20, CMFlag.BY_H)));
+        Ketchup.setOpaque(false);
+        Ketchup.setContentAreaFilled(false);
+        Ketchup.setBorderPainted(false);
+        contentPane.add(Ketchup);
+
+        JButton Lettuce = new JButton();
+        cm.setIcon(Lettuce, imageLoader.getIcon("Lettuce"), CM.size(20, CMFlag.BY_H));
+        cm.setBounds(Lettuce, CM.grid(48, 32, CM.size(20, CMFlag.BY_H)));
+        Lettuce.setOpaque(false);
+        Lettuce.setContentAreaFilled(false);
+        Lettuce.setBorderPainted(false);
+        contentPane.add(Lettuce);
+
+        JButton Meat = new JButton();
+        cm.setIcon(Meat, imageLoader.getIcon("Meat"), CM.size(20, CMFlag.BY_H));
+        cm.setBounds(Meat, CM.grid(30, 60, CM.size(20, CMFlag.BY_H)));
+        Meat.setOpaque(false);
+        Meat.setContentAreaFilled(false);
+        Meat.setBorderPainted(false);
+        contentPane.add(Meat);
+
+        JButton Potato = new JButton();
+        cm.setIcon(Potato, imageLoader.getIcon("Potato"), CM.size(20, CMFlag.BY_H));
+        cm.setBounds(Potato, CM.grid(48, 60, CM.size(20, CMFlag.BY_H)));
+        Potato.setOpaque(false);
+        Potato.setContentAreaFilled(false);
+        Potato.setBorderPainted(false);
+        contentPane.add(Potato);
+
+        JButton Rice = new JButton();
+        cm.setIcon(Rice, imageLoader.getIcon("Rice"), CM.size(20, CMFlag.BY_H));
+        cm.setBounds(Rice, CM.grid(68, 60, CM.size(20, CMFlag.BY_H)));
+        Rice.setOpaque(false);
+        Rice.setContentAreaFilled(false);
+        Rice.setBorderPainted(false);
+        contentPane.add(Rice);
+
+        cm.recalculate();
+
+        contentPane.revalidate();
+        contentPane.repaint();
+
+        revalidate();
+        repaint();
+        setVisible(true);
+    }
+}
--- src/game/scenes/gameScene1.java
+++ src/game/scenes/gameScene1.java
@@ -4,11 +4,13 @@ import base.Controller;
 import base.Scene;
 import base.Window;
 import components.CustomImageIcon;
+import game.frames.RefrigeratorFrame;
 import utility.bounding.BoundingArea;
 import utility.cm.CM;
 import utility.cm.CMFlag;
 import utility.cm.DimensionStore;
 import utility.cm.PointStore;
+import utility.loader.AudioLoader;
 import utility.loader.ImageLoader;
 
 import javax.swing.*;
@@ -28,12 +30,12 @@ public class gameScene1 extends Scene {
         imageLoader.add("background", "resources/gameScene/background.png");
         imageLoader.add("head", "resources/gameScene/Iconplayer.png");
         imageLoader.add("refrigerator", "resources/gameScene/Fridge.png");
-        imageLoader.add("bar","resources/gameScene/bar.png");
-        imageLoader.add("plate","resources/gameScene/plate.png");
-        imageLoader.add("Cutting","resources/gameScene/Cutting.png");
-        imageLoader.add("Drinking","resources/gameScene/Drinking.png");
-        imageLoader.add("pot","resources/gameScene/pot.png");
-        imageLoader.add("pan","resources/gameScene/pan.png");
+        imageLoader.add("bar", "resources/gameScene/bar.png");
+        imageLoader.add("plate", "resources/gameScene/plate.png");
+        imageLoader.add("Cutting", "resources/gameScene/Cutting.png");
+        imageLoader.add("Drinking", "resources/gameScene/Drinking.png");
+        imageLoader.add("pot", "resources/gameScene/pot.png");
+        imageLoader.add("pan", "resources/gameScene/pan.png");
         imageLoader.add("backgroundFrame", "resources/refrigeratorFrame/Bgfridge.png");
         imageLoader.add("Burger", "resources/refrigeratorFrame/Burger.png");
         imageLoader.add("Fish", "resources/refrigeratorFrame/Fish.png");
@@ -53,7 +55,6 @@ public class gameScene1 extends Scene {
     private JLabel Drinking = new JLabel();
     private JLabel pot = new JLabel();
     private JLabel pan = new JLabel();
-    private JFrame refrigeratorFrame = new JFrame();
     private boolean pickupBurger = false;
 
     private HashMap<String, Boolean> interactable = new HashMap<>();
@@ -75,35 +76,35 @@ public class gameScene1 extends Scene {
         add(refrigerator);
         map.add("refrigerator", refrigerator);
 
-        cm.setIcon(plate,imageLoader.getIcon("plate"),CM.size(10,CMFlag.BY_H));
-        cm.setBounds(plate,CM.grid(30,29,CM.size(10, CMFlag.BY_H)));
+        cm.setIcon(plate, imageLoader.getIcon("plate"), CM.size(10, CMFlag.BY_H));
+        cm.setBounds(plate, CM.grid(30, 29, CM.size(10, CMFlag.BY_H)));
         add(plate);
-        map.add("plate",plate);
+        map.add("plate", plate);
 
-        cm.setIcon(bar,imageLoader.getIcon("bar"),CM.size(44,37));
-        cm.setBounds(bar,CM.grid(24,29,44,37));
+        cm.setIcon(bar, imageLoader.getIcon("bar"), CM.size(44, 37));
+        cm.setBounds(bar, CM.grid(24, 29, 44, 37));
         add(bar);
-        map.add("bar",bar);
+        map.add("bar", bar);
 
-        cm.setIcon(Cutting,imageLoader.getIcon("Cutting"),CM.size(7,13));
-        cm.setBounds(Cutting,CM.grid(0,28,7,15));
+        cm.setIcon(Cutting, imageLoader.getIcon("Cutting"), CM.size(7, 13));
+        cm.setBounds(Cutting, CM.grid(0, 28, 7, 15));
         add(Cutting);
-        map.add("Cutting",Cutting);
+        map.add("Cutting", Cutting);
 
-        cm.setIcon(Drinking,imageLoader.getIcon("Drinking"),CM.size(7,22));
-        cm.setBounds(Drinking,CM.grid(0,50,7,22));
+        cm.setIcon(Drinking, imageLoader.getIcon("Drinking"), CM.size(7, 22));
+        cm.setBounds(Drinking, CM.grid(0, 50, 7, 22));
         add(Drinking);
-        map.add("Drinking",Drinking);
+        map.add("Drinking", Drinking);
 
-        cm.setIcon(pot,imageLoader.getIcon("pot"),CM.size(15,12));
-        cm.setBounds(pot,CM.grid(34,82,15,12));
+        cm.setIcon(pot, imageLoader.getIcon("pot"), CM.size(15, 12));
+        cm.setBounds(pot, CM.grid(34, 82, 15, 12));
         add(pot);
-        map.add("pot",pot);
+        map.add("pot", pot);
 
-        cm.setIcon(pan,imageLoader.getIcon("pan"),CM.size(14,CMFlag.BY_H));
-        cm.setBounds(pan,CM.grid(54,82,CM.size(14, CMFlag.BY_H)));
+        cm.setIcon(pan, imageLoader.getIcon("pan"), CM.size(14, CMFlag.BY_H));
+        cm.setBounds(pan, CM.grid(54, 82, CM.size(14, CMFlag.BY_H)));
         add(pan);
-        map.add("pan",pan);
+        map.add("pan", pan);
 
         map.addIntersectionListener((name) -> {
             interactable.put(name, true);
@@ -159,74 +160,8 @@ public class gameScene1 extends Scene {
 
 
         if (Boolean.TRUE.equals(interactable.get("refrigerator")) && isActionKeyPressed) {
-            System.out.println("Hi ref");
+            JFrame refrigeratorFrame = new RefrigeratorFrame(getImageLoader(), getAudioLoader());
             refrigeratorFrame.setVisible(true);
-            refrigeratorFrame.setSize(new Dimension(1200, 900));
-            refrigeratorFrame.setResizable(false);
-            setLayout(new BorderLayout());
-            JLabel background=new JLabel(getImageLoader().getIcon("backgroundFrame"));
-            refrigeratorFrame.add(background);
-            background.setLayout(new FlowLayout());
-            JButton Burger = new JButton(getImageLoader().getIcon("Burger").resize(sizeByH(0.13)));
-            cm.setIcon(Burger, getImageLoader().getIcon("Burger"), CM.size(30, CMFlag.BY_H));
-            cm.setBounds(Burger, CM.grid(30, 50,  CM.size(30, CMFlag.BY_H)));
-            Burger.setOpaque(false);
-            Burger.setContentAreaFilled(false);
-            Burger.setBorderPainted(false);
-           /* Burger.addWindowListener(new WindowAdapter(){
-                public void windowClosing(WindowEvent e){
-                }*/
-            refrigeratorFrame.add(Burger);
-
-            JButton Fish = new JButton(getImageLoader().getIcon("Fish").resize(sizeByH(0.13)));
-            cm.setIcon(Fish, getImageLoader().getIcon("Fish"), CM.size(20, CMFlag.BY_H));
-            cm.setBounds(Fish, CM.grid(48, 5,  CM.size(20, CMFlag.BY_H)));
-            Fish.setOpaque(false);
-            Fish.setContentAreaFilled(false);
-            Fish.setBorderPainted(false);
-            add(Fish);
-
-            JButton Ketchup = new JButton(getImageLoader().getIcon("Ketchup").resize(sizeByH(0.13)));
-            cm.setIcon(Ketchup, getImageLoader().getIcon("Ketchup"), CM.size(20, CMFlag.BY_H));
-            cm.setBounds(Ketchup, CM.grid(30, 32,  CM.size(20, CMFlag.BY_H)));
-            Ketchup.setOpaque(false);
-            Ketchup.setContentAreaFilled(false);
-            Ketchup.setBorderPainted(false);
-            add(Ketchup);
-
-            JButton Lettuce = new JButton(getImageLoader().getIcon("Lettuce").resize(sizeByH(0.13)));
-            cm.setIcon(Lettuce, getImageLoader().getIcon("Lettuce"), CM.size(20, CMFlag.BY_H));
-            cm.setBounds(Lettuce, CM.grid(48, 32,  CM.size(20, CMFlag.BY_H)));
-            Lettuce.setOpaque(false);
-            Lettuce.setContentAreaFilled(false);
-            Lettuce.setBorderPainted(false);
-            add(Lettuce);
-
-            JButton Meat = new JButton(getImageLoader().getIcon("Meat").resize(sizeByH(0.13)));
-            cm.setIcon(Meat, getImageLoader().getIcon("Meat"), CM.size(20, CMFlag.BY_H));
-            cm.setBounds(Meat, CM.grid(30, 60,  CM.size(20, CMFlag.BY_H)));
-            Meat.setOpaque(false);
-            Meat.setContentAreaFilled(false);
-            Meat.setBorderPainted(false);
-            add(Meat);
-
-            JButton Potato = new JButton(getImageLoader().getIcon("Potato").resize(sizeByH(0.13)));
-            cm.setIcon(Potato, getImageLoader().getIcon("Potato"), CM.size(20, CMFlag.BY_H));
-            cm.setBounds(Potato, CM.grid(48, 60,  CM.size(20, CMFlag.BY_H)));
-            Potato.setOpaque(false);
-            Potato.setContentAreaFilled(false);
-            Potato.setBorderPainted(false);
-            add(Potato);
-
-            JButton Rice = new JButton(getImageLoader().getIcon("Rice").resize(sizeByH(0.13)));
-            cm.setIcon(Rice, getImageLoader().getIcon("Rice"), CM.size(20, CMFlag.BY_H));
-            cm.setBounds(Rice, CM.grid(68, 60,  CM.size(20, CMFlag.BY_H)));
-            Rice.setOpaque(false);
-            Rice.setContentAreaFilled(false);
-            Rice.setBorderPainted(false);
-            add(Rice);
-
-
         }
         if (Boolean.TRUE.equals(interactable.get("plate")) && isActionKeyPressed) System.out.println("kuy");
     }
